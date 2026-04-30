249154224d890daae7a20c4a13325becb60c5669
neelsrkr123-rgb <neelsrkr123@gmail.com>
2026-04-30 18:57:39 +0530

    Remove all except app folder

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 border-t border-gray-700 py-8">
-      <div className="container mx-auto px-4 text-center">
-        <p className="text-gray-400">© 2024 CinePlanter. all rights resrved</p>
-      </div>
-    </footer>
-  )
-}
