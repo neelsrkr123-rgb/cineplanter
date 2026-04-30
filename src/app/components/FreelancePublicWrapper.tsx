69d52eb633446bd2bf292ea8ae3d347973275bab
neelsrkr123-rgb <neelsrkr123@gmail.com>
2026-04-30 20:21:52 +0530

    Remove duplicate home page

--- src/app/components/FreelancePublicWrapper.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import React from "react";
-import ShareableIDCard from "#/components/ShareableIDCard";
-import type { ExtendedUser } from "#/types/user";
-
-export default function FreelancerPublicWrapper({ user }: { user: Partial<ExtendedUser> }) {
-  return <ShareableIDCard user={user} readOnly />;
-}
